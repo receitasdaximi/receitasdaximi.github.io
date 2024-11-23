9f5b40bdd74ac730e6129ff49dd1ee982be0aa46
williamguilhermesouza <williamguilhermesouza@gmail.com>
2024-11-23 13:15:35 -0300

    working on actions to deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/app/globals.css";
 import { useEffect } from "react";
+import type { AppProps } from "next/app";
 
-function ReceitasDaXimiWeb({ Component, pageProps }) {
+function ReceitasDaXimiWeb({Component, pageProps} : AppProps) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
