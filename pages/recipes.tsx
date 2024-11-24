43597c14394d75cc77323ea54f04df4e3d4fb9e3
williamguilhermesouza <williamguilhermesouza@gmail.com>
2024-11-24 14:57:21 -0300

    added search in recipes list and minor changes

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState, useMemo } from "react";
 export default function Recipes() {
     const database = useMemo(() =>  new Database(), []);
     const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([]);
     
     useEffect(() => {
         database.getAll().then(value => {
@@ -16,6 +17,7 @@ export default function Recipes() {
                 return;
     
             setRecipes(value);
+            setFilteredRecipes(value);
         });
     }, [database]);
 
@@ -27,15 +29,26 @@ export default function Recipes() {
     return(
         <div className="d-flex flex-column w-100">
             <h1 className="align-self-center">Receitas</h1>
+            <input type="text" name="search-recipe" id="search-recipe" className="form-control m-2 my-4 w-75 align-self-center" onChange={(e) => {
+                const inputValue = e.target.value;
+                if (e.target.value === '') {
+                    setFilteredRecipes(recipes);
+                    return;
+                }
+                setFilteredRecipes(recipes.filter(recipe => recipe.name.includes(inputValue)));
+            }} />
             <ul className="recipesList">
             {
-                recipes.map((recipe: Recipe) => 
-                    <li key={recipe.id} className="d-flex justify-content-between align-items-center me-4">
-                        <Link 
-                            href={`/new/${recipe.id}`}
-                            className="text-decoration-none fs-4">{recipe.name}</Link>
-                        <button className="btn-close" onClick={() => recipe.id ? deleteFromDb(recipe.id) : ''}></button>
-                    </li>
+                filteredRecipes.map((recipe: Recipe) => 
+                    <div key={recipe.id}>
+                        <li className="d-flex justify-content-between align-items-center me-4">
+                            <Link 
+                                href={`/new/${recipe.id}`}
+                                className="text-decoration-none fs-4">{recipe.name}</Link>
+                            <button className="btn-close" onClick={() => recipe.id ? deleteFromDb(recipe.id) : ''}></button>
+                        </li>
+                        <hr className="me-4"/>
+                    </div>
                 )
             }
             </ul>
