e37999defb872fa51f9e9f5e263aa5da408b4897
williamguilhermesouza <williamguilhermesouza@gmail.com>
2025-08-02 20:08:33 -0300

    putting import button inside accordion

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ import Database from "@/services/database";
 export default function Home() {
   const router = useRouter();
   const [fileToImport, setFileToImport] = useState<File | null>(null);
-  const database = useMemo(() =>  new Database(), []);
+  const database = useMemo(() => new Database(), []);
 
   const handleImportFormSubmit: FormEventHandler<HTMLFormElement> = async (
     event: FormEvent
@@ -60,13 +60,14 @@ export default function Home() {
 
   const handleExportAllButtonClick: MouseEventHandler = async () => {
     const allRecipes: Recipe[] = await database.getAll();
-    const recipesAsOldRecipe: OldRecipe[] = OldRecipe.ParseFromRecipe(allRecipes);
+    const recipesAsOldRecipe: OldRecipe[] =
+      OldRecipe.ParseFromRecipe(allRecipes);
     const jsonString = JSON.stringify(recipesAsOldRecipe, null);
     const blob = new Blob([jsonString], { type: "application/json" });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
     link.href = url;
-    link.download = "receitas.json"; 
+    link.download = "receitas.json";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -75,9 +76,7 @@ export default function Home() {
 
   return (
     <div className="d-flex flex-column indexPage">
-      <Image 
-        src={Logo} 
-        alt="Logo" />
+      <Image src={Logo} alt="Logo" />
       <button
         className="btn btn-info indexButton m-2 mt-4"
         onClick={() => router.push("/recipes")}
@@ -94,24 +93,46 @@ export default function Home() {
         className="btn btn-info indexButton m-2"
         onClick={handleExportAllButtonClick}
       >
-        Exportar receitas
+        Exportar todas as receitas
       </button>
-      <form
-        className="d-flex flex-column form py-4 row-gap-3"
-        onSubmit={(e) => handleImportFormSubmit(e)}
-      >
-        <input
-          type="file"
-          accept="application/JSON"
-          className="form-control"
-          onChange={(e) =>
-            setFileToImport(e.target.files ? e.target.files[0] : null)
-          }
-        />
-        <button className="btn btn-info indexButton m-2" type="submit">
-          Importar receitas
-        </button>
-      </form>
+      <div className="accordion indexButton m-2" id="importAccordion" style={{width: "400px" }}>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button btn-info fw-bold text-light collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="false"
+              aria-controls="collapseOne"
+            >
+              Importar receitas
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#importAccordion"
+          >
+            <form
+              className="d-flex flex-column form p-4 row-gap-3"
+              onSubmit={(e) => handleImportFormSubmit(e)}
+            >
+              <input
+                type="file"
+                accept="application/JSON"
+                className="form-control mx-auto"
+                onChange={(e) =>
+                  setFileToImport(e.target.files ? e.target.files[0] : null)
+                }
+              />
+              <button className="btn btn-secondary mx-auto" type="submit">
+                Importar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
